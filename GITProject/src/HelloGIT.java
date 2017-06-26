d5ca35741110f61fc4ba0399fa95a92780bfeb14
RuiFilipeGarcia <ruifilipegarcia@gmail.com>
2017-06-26 20:00:25 +0100

    Add HelloGIT class

---
+++ GITProject/src/HelloGIT.java
@@ -0,0 +1,16 @@
+
+/*******************************************
+ * HelloGIT.java
+ * Rui Garcia
+ *
+ * This program prints a HelloGIT message.
+ */
+public class HelloGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Hello GIT!");
+	}
+
+}
